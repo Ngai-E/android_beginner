fe2de73d263951aaea8c78435ae632f147ac3f90
Elizabeth <asobingai@gmail.com>
2019-10-20 22:10:23 +0100

    initial commit

---
+++ app/src/main/java/com/example/justjavaapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.justjavaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+    int count = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        display(2);
+        displayPrice(2 * 5);
+        count++;
+    }
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(number + "");
+    }
+
+    private  void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
